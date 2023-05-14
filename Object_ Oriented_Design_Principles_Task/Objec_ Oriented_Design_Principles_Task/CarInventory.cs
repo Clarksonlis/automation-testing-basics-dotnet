8a59ab40e22e135f457b332c5dac111095aeccff
daryakaptsiuh <clarksonlis1999@gmail.com>
2023-05-14 20:28:57 +0400

    Object Oriented Design Principles

---
+++ Object_ Oriented_Design_Principles_Task/Objec_ Oriented_Design_Principles_Task/CarInventory.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace ObjectOrientedDesignPrinciples_Task
+{
+    public class CarInventory
+    {
+        private readonly Dictionary<string, List<Car>> inventory;
+
+        public CarInventory()
+        {
+            inventory = new Dictionary<string, List<Car>>();
+        }
+
+        public void AddCar(Car car)
+        {
+            if (inventory.ContainsKey(car.Brand))
+            {
+                inventory[car.Brand].Add(car);
+            }
+            else
+            {
+                inventory.Add(car.Brand, new List<Car> { car });
+            }
+        }
+
+        public int CountAll()
+        {
+            int count = 0;
+
+            foreach (var carList in inventory.Values)
+            {
+                count += carList.Count;
+            }
+
+            return count;
+        }
+
+        public int CountTypes()
+        {
+            return inventory.Count;
+        }
+
+        public decimal GetAveragePrice()
+        {
+            if (CountAll() == 0)
+            {
+                return 0;
+            }
+
+            decimal totalCost = 0;
+
+            foreach (var carList in inventory.Values)
+            {
+                foreach (var car in carList)
+                {
+                    totalCost += car.Cost;
+                }
+            }
+
+            return totalCost / CountAll();
+        }
+
+        public decimal GetAveragePriceType(string brand)
+        {
+            if (!inventory.ContainsKey(brand) || inventory[brand].Count == 0)
+            {
+                return 0;
+            }
+
+            decimal totalCost = 0;
+
+            foreach (var car in inventory[brand])
+            {
+                totalCost += car.Cost;
+            }
+
+            return totalCost / inventory[brand].Count;
+        }
+    }
+}
