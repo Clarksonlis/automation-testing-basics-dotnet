cee4dc157a89130418190bc2a78332a440a4fa61
daryakaptsiuh <clarksonlis1999@gmail.com>
2023-05-14 20:20:39 +0400

    Interfaces and Abstract Classes

---
+++ Interfaces_Abstract_Classes_Task/Interfaces_Abstract_Classes_Task/Coordinate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Interfaces_Abstract_Classes_Task
+{
+	public struct Coordinate
+	{
+		public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+
+        public double DistanceTo(Coordinate coordinate)
+        {
+            double  newX = coordinate.X - this.X;
+            double  newY = coordinate.Y - this.Y;
+            double  newZ = coordinate.Z - this.Z;
+
+            newX *= newX;
+            newY *= newY;
+            newZ *= newZ;
+
+
+
+            double result = Math.Sqrt(newX + newY + newZ);
+            return result;
+
+        }
+    }
+}
+
---
+++ Interfaces_Abstract_Classes_Task/Interfaces_Abstract_Classes_Task/IFlyable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Interfaces_Abstract_Classes_Task
+{
+	public interface IFlyable
+	{
+		public void FlyTo(Coordinate coordinate);
+		public double GetFlyTime(Coordinate coordinate);
+
+    }
+}
+
