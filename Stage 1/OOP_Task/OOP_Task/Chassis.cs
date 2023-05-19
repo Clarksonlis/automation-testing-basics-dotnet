b2e67e09e0ec3c93a00a205abe2e089ae3885813
daryakaptsiuh <clarksonlis1999@gmail.com>
2023-05-19 21:04:02 +0400

    Add OOP_Task

---
+++ Stage 1/OOP_Task/OOP_Task/Chassis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP_Task
+{
+	public class Chassis
+	{
+        public int wheelsNumber;
+        public string number;
+        public string permissibleLoad;
+
+        public Chassis(int chassisWheelsNumber, string chassisNumber, string chassisPermissibleLoad)
+        {
+            wheelsNumber = chassisWheelsNumber;
+            number = chassisNumber;
+            permissibleLoad = chassisPermissibleLoad;
+        }
+    }
+}
---
+++ Stage 1/OOP_Task/OOP_Task/Transmission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP_Task
+{
+	public class Transmission
+	{
+        public string type;
+        public int numberOfGears;
+        public string manufacturer;
+
+        public Transmission(string transmissionType, int transmissionNumberOfGears, string transmissionManufacturer)
+        {
+            type = transmissionType;
+            numberOfGears = transmissionNumberOfGears;
+            manufacturer = transmissionManufacturer;
+        }
+    }
+}
+
