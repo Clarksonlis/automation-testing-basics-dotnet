5b0fb542ed8e65b8132e64a166051010bd5033f3
daryakaptsiuh <clarksonlis1999@gmail.com>
2023-05-20 15:18:54 +0400

    Add comments

--- Stage 1/BasicFramework_Task/BasicFramework_Task/Program.cs
+++ Stage 1/BasicFramework_Task/BasicFramework_Task/Program.cs
@@ -4,33 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Введите десятичное число:");
+        Console.WriteLine("Enter decimal number:");
         string decimalNumberInput = Console.ReadLine();
 
+        // Parse the decimal number input into an integer
         int decimalNumber;
         if (!int.TryParse(decimalNumberInput, out decimalNumber))
         {
-            Console.WriteLine("Некорректное десятичное число.");
+            Console.WriteLine("Invalid decimal number.");
             return;
         }
 
-        Console.WriteLine("Введите основание системы счисления (от 2 до 20):");
+        Console.WriteLine("Enter the base of the number system (from 2 to 20):");
         string baseInput = Console.ReadLine();
 
+        // Parse the base input into an integer and validate it
         int newBase;
         if (!int.TryParse(baseInput, out newBase) || newBase < 2 || newBase > 20)
         {
-            Console.WriteLine("Некорректное основание системы счисления. Основание должно быть от 2 до 20.");
+            Console.WriteLine("Incorrect base of the number system. The base must be between 2 and 20.");
             return;
         }
 
         string result = ConvertToBase(decimalNumber, newBase);
-        Console.WriteLine($"Десятичное число {decimalNumber} в системе счисления с основанием {newBase}: {result}");
+        Console.WriteLine($"Decimal number {decimalNumber} in base number system {newBase}: {result}");
     }
 
+    // Converts the decimal number to the specified base
     static string ConvertToBase(int decimalNumber, int newBase)
     {
-        string digits = "0123456789ABCDEFGHIJ"; // Цифры для оснований до 20
+        string digits = "0123456789ABCDEFGHIJ";
         string result = "";
 
         while (decimalNumber > 0)
