5ad4efcacff7cb7d6a1d1f96a1c00d7f46fd99c2
daryakaptsiuh <clarksonlis1999@gmail.com>
2023-05-20 21:50:05 +0400

    Add UnitTests

--- Stage 1/DevelopmentAndBuildTools_Task/DevelopmentAndBuildTools_Task/Program.cs
+++ Stage 1/DevelopmentAndBuildTools_Task/DevelopmentAndBuildTools_Task/Program.cs
@@ -1,55 +1,51 @@
 ﻿using System;
 
-namespace EpamApp1
+namespace DevelopmentAndBuildTools;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Input symbols: \n");
+        string input = Console.ReadLine();
+
+        int maxConsecutiveUnequalChars = GetMaxConsecutiveUnequalChars(input);
+
+        Console.WriteLine($"The maximum number of unequal consecutive characters is: {maxConsecutiveUnequalChars}");
+    }
+
+    // Method to calculate the maximum number of consecutive unequal characters
+    public static int GetMaxConsecutiveUnequalChars(string input)
+    {
+        char[] inputArray = input.ToCharArray();
+
+        if (inputArray.Length == 0)
         {
-            Console.WriteLine("Input symbols: \n");
-            string input = Console.ReadLine();
-
-            int maxConsecutiveUnequalChars = GetMaxConsecutiveUnequalChars(input);
-
-            Console.WriteLine($"The maximum number of unequal consecutive characters is: {maxConsecutiveUnequalChars}");
+            return 0; 
         }
 
-        // Method to calculate the maximum number of consecutive unequal characters
-        private static int GetMaxConsecutiveUnequalChars(string input)
+        int currentNumber = 1;
+        int maxNumber = 1;
+        int startIndex = 0;
+
+        for (int i = 1; i < inputArray.Length; i++)
         {
-            char[] inputArray = input.ToCharArray();
-
-            if (inputArray.Length == 0)
+            for (int j = i - 1; j >= startIndex; j--)
             {
-                return 0; 
-            }
-
-            int currentNumber = 1;
-            int maxNumber = 1;
-            int startIndex = 0;
-
-            for (int i = 1; i < inputArray.Length; i++)
-            {
-                for (int j = i - 1; j >= startIndex; j--)
+                // If a repeating character is found, update the maximum count and reset the current count
+                if (inputArray[j] == inputArray[i])
                 {
-                    // If a repeating character is found, update the maximum count and reset the current count
-                    if (inputArray[j] == inputArray[i])
-                    {
-                        maxNumber = currentNumber > maxNumber ? currentNumber : maxNumber;
-                        currentNumber = 0;
-                        startIndex = i;
-                        break;
-                    }
-
+                    maxNumber = currentNumber > maxNumber ? currentNumber : maxNumber;
+                    currentNumber = 0;
+                    startIndex = i;
+                    break;
                 }
-                currentNumber++;
-                maxNumber = currentNumber > maxNumber ? currentNumber : maxNumber;
-            }
 
-            return maxNumber;
+            }
+            currentNumber++;
+            maxNumber = currentNumber > maxNumber ? currentNumber : maxNumber;
         }
+
+        return maxNumber;
     }
 }
-
-
-
---
+++ Stage 1/DevelopmentAndBuildTools_Task/DevelopmentAndBuildTools_UnitTests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using DevelopmentAndBuildTools;
+
+namespace DevelopmentAndBuildTools_UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase("", 0)]
+    [TestCase(" ", 1)]
+    [TestCase("abc", 3)]
+    [TestCase("abcd", 4)]
+    [TestCase("AabB", 4)]
+    public void GetMaxConsecutiveUnequalChars_StringInput_CharactersLength(string input, int expectedResult)
+    {
+        int result = Program.GetMaxConsecutiveUnequalChars(input);
+
+        Assert.AreEqual(expectedResult, result);
+    }
+}
